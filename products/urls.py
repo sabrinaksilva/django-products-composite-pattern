244c3e291982fb7129f4eb5db99a926a48c2e679
sabrinaksilva <sabrinakappann@gmail.com>
2024-12-04 09:43:23 -0300

    get_product_by_id form and view

--- products/urls.py
+++ products/urls.py
@@ -5,8 +5,9 @@ from . import views
 urlpatterns = [
     path('', views.foo, name='foo'),
 
-    path('dashboard/', views.dashboard, name='home'),
-    path('products/', views.products, name='products'),
+    path('products/', views.list_products, name='products'),
+    path('products/<str:pk>/', views.get_product, name='get_product'),
+
     path('products/new', views.create_product, name='create_product'),
 
     path('foo/', views.foo, name='foo'),
--- products/views.py
+++ products/views.py
@@ -5,11 +5,7 @@ from products.forms import ProductForm
 from products.services.product_service import ProductService
 
 
-def dashboard(request):
-    return render(request, 'products/dashboard.html')
-
-
-def products(request):
+def list_products(request):
     all_products = ProductService.get_products()
     context = {'products': all_products}
     return render(request, 'products/products.html', context)
@@ -29,6 +25,24 @@ def create_product(request):
 
     else:
         form = ProductForm()
+
+    context = {'form': form}
+    return render(request, 'products/product-form.html', context)
+
+
+def get_product(request, pk):
+    product = ProductService.get_product_by_id(pk)
+
+    if request.method == 'POST':
+        form = ProductForm(request.POST, instance=product)
+        if form.is_valid():
+            form.save()
+            return redirect('products')
+        else:
+            print("Invalid form:")
+            print(form.errors)
+    else:
+        form = ProductForm(instance=product)
 
     context = {'form': form}
     return render(request, 'products/product-form.html', context)
